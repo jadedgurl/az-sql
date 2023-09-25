56b59ad7ce7256ae37add8eec71ad07d4a6db04b
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-09-25 19:30:26 -0400

    Update kubernates.tf

--- kubernates.tf
+++ kubernates.tf
@@ -1,3 +1,21 @@
 locals{
   clusters_name= ["jade","sajeevan","paul","douglas","emannuel","olarewaju","oladi"]
 }
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  for_each            = { for cluster in local.cluster_names : cluster => cluster }
+  name                = "k8s-${var.convention}-${each.key}"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "${var.prefix}-k8s"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
